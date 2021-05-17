0f7da5a8f42755bfdef2f5b2d2a5b4e0f57a4a98
Helmut Siegel <helmutsiegel@yahoo.com>
2021-05-17 20:06:24 +0300

    tabs in angular frontend

--- web-client/src/main/js/profile/src/app/app-routing.module.ts
+++ web-client/src/main/js/profile/src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WelcomeComponent} from "./home/welcome/welcome.component";
+import {CvComponent} from "./cv/cv/cv.component";
+import {ResumeComponent} from "./resume/resume/resume.component";
+import {ProjectsComponent} from "./projects/projects/projects.component";
 
 const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  {
+    path: 'cv',
+    component: CvComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
 
--- web-client/src/main/js/profile/src/app/cv/cv.module.ts
+++ web-client/src/main/js/profile/src/app/cv/cv.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CvComponent } from './cv/cv.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CvComponent
+  ],
   imports: [
     CommonModule
   ]
