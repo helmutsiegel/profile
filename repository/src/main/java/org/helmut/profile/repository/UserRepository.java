287fea41feb2160ce453c355fcd09e17339a837c
Helmut Siegel <helmutsiegel@yahoo.com>
2021-05-17 16:04:07 +0300

    Setups

--- repository/src/main/java/org/helmut/profile/repository/UserRepository.java
+++ repository/src/main/java/org/helmut/profile/repository/UserRepository.java
@@ -4,14 +4,12 @@ import org.helmut.profile.repository.entity.UserEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 public class UserRepository {
 
     @PersistenceContext(unitName = "profile-unit")
     private EntityManager em;
 
-    @Transactional(Transactional.TxType.NEVER)
     public UserEntity findById(Long id) {
         return em.find(UserEntity.class, id);
     }
