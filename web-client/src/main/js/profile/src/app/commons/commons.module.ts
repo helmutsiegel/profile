6151c6b7d11f8b764e2fd53b9ef7d0735ac968ee
Helmut Siegel <helmutsiegel@yahoo.com>
2021-06-02 07:37:31 +0300

    collapsible card

--- web-client/src/main/js/profile/src/app/commons/commons.module.ts
+++ web-client/src/main/js/profile/src/app/commons/commons.module.ts
@@ -3,20 +3,23 @@ import {CommonModule} from '@angular/common';
 import {UsersNavbarComponent} from "./component/users-navbar/users-navbar.component";
 import {RouterModule} from "@angular/router";
 import { Error404Component } from './component/error404/error404.component';
+import { CollapsibleCardComponent } from './component/collapsible-card/collapsible-card.component';
 
 
 @NgModule({
   declarations: [
     UsersNavbarComponent,
-    Error404Component
+    Error404Component,
+    CollapsibleCardComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [
-    UsersNavbarComponent
-  ]
+    exports: [
+        UsersNavbarComponent,
+        CollapsibleCardComponent
+    ]
 })
 export class CommonsModule {
 }
