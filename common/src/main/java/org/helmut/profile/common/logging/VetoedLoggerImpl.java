e8726d985024dff0cd63b7e70e539beba84e5d7e
Helmut Siegel <helmutsiegel@yahoo.com>
2021-07-07 10:13:09 +0300

    Class comment

--- common/src/main/java/org/helmut/profile/common/logging/VetoedLoggerImpl.java
+++ common/src/main/java/org/helmut/profile/common/logging/VetoedLoggerImpl.java
@@ -7,7 +7,7 @@ import javax.enterprise.inject.Vetoed;
  * This class is created just as an example for cdi vetoed
  * Currently we have two implementation for Logger with default qualifier in this package: {@link DefaultLoggerImpl} and this class
  * If we do not add the @Vetoed annotation to either of the mentioned classes,
- * then by deploying the application we will get and DeploymentException with the message 'Ambiguous dependencies for type Logger...'
+ * then by deploying the application we will get and DeploymentException with the message 'Ambiguous dependencies for type  Logger with qualifiers @Default'
  */
 @Vetoed
 public class VetoedLoggerImpl implements Logger {
