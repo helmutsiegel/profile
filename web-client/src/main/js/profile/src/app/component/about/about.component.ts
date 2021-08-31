717bc62aaf100a9b2a14d996ed4ce8e73d1afde8
Helmut Siegel <helmutsiegel17@gmail.com>
2021-08-31 11:55:51 +0300

    Added new course to the courses list

--- web-client/src/main/js/profile/src/app/component/about/about.component.ts
+++ web-client/src/main/js/profile/src/app/component/about/about.component.ts
@@ -31,37 +31,45 @@ export class AboutComponent implements OnInit {
     platform: 'Pluralsight',
     link: 'https://app.pluralsight.com/library/courses/context-dependency-injection-1-1/table-of-contents',
     status: 'Finished'
-  }, {
-    title: 'Getting Started Unit Testing with JUnit 5',
-    author: 'Jim Weaver',
-    platform: 'Pluralsight',
-    link: 'https://app.pluralsight.com/library/courses/junit-5-unit-testing-getting-started/table-of-contents',
-    status: 'In progress'
-  }, {
-    title: 'RESTFul Services in Java using Jersey',
-    author: 'Bryan Hansen',
-    platform: 'Pluralsight',
-    link: 'https://app.pluralsight.com/library/courses/restful-services-java-using-jersey/table-of-contents',
-    status: 'Planned'
-  }, {
-    title: 'Maven Fundamentals',
-    author: 'Bryan Hansen',
-    platform: 'Pluralsight',
-    link: 'https://app.pluralsight.com/library/courses/maven-fundamentals/table-of-contents',
-    status: 'Planned'
-  }, {
-    title: 'SOLID Software Design Principles in Java',
-    author: 'Dan Geabunea',
-    platform: 'Pluralsight',
-    link: 'https://app.pluralsight.com/library/courses/solid-software-design-principles-java/table-of-contents',
-    status: 'Planned'
-  }, {
-    title: 'Jenkins',
-    author: '',
-    platform: '',
-    link: '',
-    status: 'Planned'
-  }];
+  },
+    {
+      title: 'Getting Started Unit Testing with JUnit 5',
+      author: 'Jim Weaver',
+      platform: 'Pluralsight',
+      link: 'https://app.pluralsight.com/library/courses/junit-5-unit-testing-getting-started/table-of-contents',
+      status: 'Finished'
+    },
+    {
+      title: 'TDD with JUnit 5',
+      author: 'Catalin Tudose',
+      platform: 'Pluralsight',
+      link: 'https://app.pluralsight.com/library/courses/tdd-junit5/table-of-contents',
+      status: 'In progress'
+    }, {
+      title: 'RESTFul Services in Java using Jersey',
+      author: 'Bryan Hansen',
+      platform: 'Pluralsight',
+      link: 'https://app.pluralsight.com/library/courses/restful-services-java-using-jersey/table-of-contents',
+      status: 'Planned'
+    }, {
+      title: 'Maven Fundamentals',
+      author: 'Bryan Hansen',
+      platform: 'Pluralsight',
+      link: 'https://app.pluralsight.com/library/courses/maven-fundamentals/table-of-contents',
+      status: 'Planned'
+    }, {
+      title: 'SOLID Software Design Principles in Java',
+      author: 'Dan Geabunea',
+      platform: 'Pluralsight',
+      link: 'https://app.pluralsight.com/library/courses/solid-software-design-principles-java/table-of-contents',
+      status: 'Planned'
+    }, {
+      title: 'Jenkins',
+      author: '',
+      platform: '',
+      link: '',
+      status: 'Planned'
+    }];
 
   constructor() {
   }
