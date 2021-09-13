ee568a44678f74133a040bbf80144d0eee32cc7a
Helmut Siegel <helmutsiegel17@gmail.com>
2021-09-13 17:51:16 +0300

    Test fixes

--- common/src/test/java/org/helmut/profile/common/ValidationUtilsTest.java
+++ common/src/test/java/org/helmut/profile/common/ValidationUtilsTest.java
@@ -11,7 +11,7 @@ import javax.validation.ValidatorFactory;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 public class ValidationUtilsTest {
@@ -24,10 +24,11 @@ public class ValidationUtilsTest {
         ValidatorFactory vf = Validation.buildDefaultValidatorFactory();
         Validator validator = vf.getValidator();
 
-        assertEquals(validationUtils.violationsToSting(validator.validate(new TestObj())),
-                "# TestObj.id - Invalid Value = null - Error message = may not be null," +
-                        "# TestObj.attribute2 - Invalid Value =  - Error message = size must be between 2 and 5," +
-                        "# TestObj.attribute1 - Invalid Value = null - Error message = may not be null");
+        String violationsToSting = validationUtils.violationsToSting(validator.validate(new TestObj()));
+
+        assertTrue(violationsToSting.contains("# TestObj.id - Invalid Value = null - Error message = may not be null"));
+        assertTrue(violationsToSting.contains("# TestObj.attribute2 - Invalid Value =  - Error message = size must be between 2 and 5"));
+        assertTrue(violationsToSting.contains("# TestObj.attribute1 - Invalid Value = null - Error message = may not be null"));
 
         vf.close();
     }
