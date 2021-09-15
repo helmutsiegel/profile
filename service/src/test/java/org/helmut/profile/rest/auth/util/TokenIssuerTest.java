8103eee09a267ad236eaf249a475d7f93f81735b
Helmut Siegel <helmutsiegel17@gmail.com>
2021-09-15 07:33:59 +0300

    Token issuer test

--- service/src/test/java/org/helmut/profile/rest/auth/util/TokenIssuerTest.java
+++ service/src/test/java/org/helmut/profile/rest/auth/util/TokenIssuerTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 class TokenIssuerTest {
 
-    @Mock
+    @Spy
     private KeyGenerator keyGenerator;
 
     @Mock
@@ -24,6 +27,9 @@ class TokenIssuerTest {
 
     @Test
     void testIssueToken() {
+        URI uri = URI.create("uri");
+        doReturn(uri).when(uriInfo).getAbsolutePath();
 
+        assertNotNull(tokenIssuer.issueToken("Subject"));
     }
 }
